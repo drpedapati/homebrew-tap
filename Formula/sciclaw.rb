161d7e495c30512276d9037886ca3451be5ccb49
Ernie Pedapati <ernest.pedapati@cchmc.org>
2026-02-25 06:22:33 -0600

    sciclaw v0.1.55

--- Formula/sciclaw.rb
+++ Formula/sciclaw.rb
@@ -1,7 +1,7 @@
 class Sciclaw < Formula
   desc "Autonomous paired scientist CLI forked from PicoClaw"
   homepage "https://github.com/drpedapati/sciclaw"
-  version "0.1.54"
+  version "0.1.55"
   license "MIT"
 
   depends_on "imagemagick"
@@ -14,27 +14,27 @@ class Sciclaw < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/drpedapati/sciclaw/releases/download/v0.1.54/sciclaw-darwin-arm64"
-      sha256 "db08122636f7c0dc51efc31d10a1be226aab3c671edee3ecd215041d25ad007c"
+      url "https://github.com/drpedapati/sciclaw/releases/download/v0.1.55/sciclaw-darwin-arm64"
+      sha256 "66692f5cdd5443e5415c555c4eb090521eb9d73548bf7d108049f5d2a7387ea8"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/drpedapati/sciclaw/releases/download/v0.1.54/sciclaw-linux-arm64"
-      sha256 "fe4a7b600b887bafbf6155f57f5c2df39907a6024f578aceeadd39b1785d69cd"
+      url "https://github.com/drpedapati/sciclaw/releases/download/v0.1.55/sciclaw-linux-arm64"
+      sha256 "b6ba7ebe945d1cd2c159e3201dd37c66cd8428765b6b0400451c011665b824cc"
     end
     on_intel do
-      url "https://github.com/drpedapati/sciclaw/releases/download/v0.1.54/sciclaw-linux-amd64"
-      sha256 "61b76f765095f61045d61dad4e4e804c69b6ee41a4566dc864848f4d35cf15e8"
+      url "https://github.com/drpedapati/sciclaw/releases/download/v0.1.55/sciclaw-linux-amd64"
+      sha256 "57866a94c5aef054e3876875943041fbdd04bfb708909867a5f41edd82785683"
     end
     depends_on "sciclaw-quarto"
   end
 
   # Source archive provides skills and workspace templates
   resource "source" do
-    url "https://github.com/drpedapati/sciclaw/archive/refs/tags/v0.1.54.tar.gz"
-    sha256 "9a49861d2f68f228f65b2a59046cef59289478ea48c36009bafbd3c97ac49bc1"
+    url "https://github.com/drpedapati/sciclaw/archive/refs/tags/v0.1.55.tar.gz"
+    sha256 "a20b9034b9d090e9abf3226c2d75cb8916fa38740cecbe34142fa9c43e553850"
   end
 
   def install
