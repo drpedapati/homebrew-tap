592d82832cfd475c7d10e72bce2114523fbb2ab9
Ernie Pedapati <ernest.pedapati@cchmc.org>
2026-03-15 10:26:44 -0400

    sciclaw-dev v0.2.4-dev.18

--- Formula/sciclaw-dev.rb
+++ Formula/sciclaw-dev.rb
@@ -1,7 +1,7 @@
 class SciclawDev < Formula
   desc "Autonomous paired scientist CLI forked from PicoClaw"
   homepage "https://github.com/drpedapati/sciclaw"
-  version "0.2.4-dev.17"
+  version "0.2.4-dev.18"
   license "MIT"
 
   depends_on "imagemagick"
@@ -17,27 +17,27 @@ class SciclawDev < Formula
 
   on_macos do
     on_arm do
-      url "https://github.com/drpedapati/sciclaw/releases/download/v0.2.4-dev.17/sciclaw-darwin-arm64"
-      sha256 "3792871ef28967d199f140e9be8577bc86551d3ff9b6b2126d387e883d57af2a"
+      url "https://github.com/drpedapati/sciclaw/releases/download/v0.2.4-dev.18/sciclaw-darwin-arm64"
+      sha256 "ac573217dcee828190f2e45bdf1e42aa8600b4b29a8b502fa2b60c569e853153"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/drpedapati/sciclaw/releases/download/v0.2.4-dev.17/sciclaw-linux-arm64"
-      sha256 "b58b342283cf831cb6564889c710d5e2d658e062feecdaad724304479b0d4788"
+      url "https://github.com/drpedapati/sciclaw/releases/download/v0.2.4-dev.18/sciclaw-linux-arm64"
+      sha256 "99a7f3311f3868c840f42b4ebb24d35154c73d8b007d84f9f18073e099b864d1"
     end
     on_intel do
-      url "https://github.com/drpedapati/sciclaw/releases/download/v0.2.4-dev.17/sciclaw-linux-amd64"
-      sha256 "c6e3572e36a48b878a36e8dac83315ecc35c7c7df299272f1ba0615820cf2fab"
+      url "https://github.com/drpedapati/sciclaw/releases/download/v0.2.4-dev.18/sciclaw-linux-amd64"
+      sha256 "391e0599dee8a694b8f760459224e5272bdabc6ba4d1b1839eaa0326258f7cd7"
     end
     depends_on "sciclaw-quarto"
   end
 
   # Source archive provides skills and workspace templates
   resource "source" do
-    url "https://github.com/drpedapati/sciclaw/releases/download/v0.2.4-dev.17/source-sciclaw-v0.2.4-dev.17-source.tar.gz"
-    sha256 "8b75176d21e06480b887b9f3840dd22427013c6291d1583943360d1eefe44cac"
+    url "https://github.com/drpedapati/sciclaw/releases/download/v0.2.4-dev.18/source-sciclaw-v0.2.4-dev.18-source.tar.gz"
+    sha256 "1e860fa22bf3f6b757baa8bb0cecf95368d285d33beead1b3a7027ecdf0c4679"
   end
 
   def install
